80f2c166c480eb3bc06dd70cea0c4302bc0a78ef
THE-VR7 <vineetrathor57@gmail.com>
2020-03-26 00:58:39 +0530

    Routing basic Level

--- ang/src/app/app-routing.module.ts
+++ ang/src/app/app-routing.module.ts
@@ -1,8 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { SliderComponent } from './slider/slider.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+import { TourpackagesComponent } from './tourpackages/tourpackages.component';
+import { PackageComponent } from './tourpackages/package/package.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/home',pathMatch:'full'},
+  { path: 'home', component: SliderComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'signup', component: SignupComponent},
+  { path: 'tourpackage', component: TourpackagesComponent},
+  { path: 'package', component: PackageComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- ang/src/app/app.module.ts
+++ ang/src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AboutusComponent } from './aboutus/aboutus.component';
@@ -9,6 +8,11 @@ import { FeaturesComponent } from './features/features.component';
 import { BannerComponent } from './banner/banner.component';
 import { FeedbackComponent } from './feedback/feedback.component';
 import { SliderComponent } from './slider/slider.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+import { TourpackagesComponent } from './tourpackages/tourpackages.component';
+import { PackageComponent } from './tourpackages/package/package.component';
+
 
 @NgModule({
   declarations: [
@@ -18,7 +22,11 @@ import { SliderComponent } from './slider/slider.component';
     FeaturesComponent,
     BannerComponent,
     FeedbackComponent,
-    SliderComponent
+    SliderComponent,
+    LoginComponent,
+    SignupComponent,
+    TourpackagesComponent,
+    PackageComponent
   ],
   imports: [
     BrowserModule,
