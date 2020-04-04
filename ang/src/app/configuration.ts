e01e3bbac16c90ee61c6fffba0ebdb3217c941ff
THE-VR7 <vineetrathor57@gmail.com>
2020-04-04 18:47:58 +0530

    loaded map api

--- ang/src/app/configuration.ts
+++ ang/src/app/configuration.ts
@@ -408,39 +408,388 @@ flight:
       },
 
 
-      // {
-      //   name: "",
-      //   duration: "",
-      //   discription: "",
-      //   sliderimg: 
-      //   [
-      //     "sydney/slider/pic1.jpg",
-      //     "sydney/slider/pic2.jpg",
-      //     "sydney/slider/pic3.jpg",
-      //   ],
-      //   hotel:{
-      //     name: "",
-      //     hotelimg:
-      //     [
-      //       "sydney/hotel/pic1.jpg",
-      //       "sydney/hotel/pic2.jpg",
-      //       "sydney/hotel/pic3.jpg",
-      //       "sydney/hotel/pic4.jpg",
-      //     ]
-      //   },
-      //   flight:
-      //   {
-      //     count: "",
-      //     timewithplace:
-      //     [
-      //       { from: " New Delhi (21:55)"  , to: "Hong Kong (06:10)"  },
-      //       { from: " Hong Kong (17:10)"  , to: "New Delhi (20:10)"  }
-      //     ]
-      //   }
-      //   },
+      {
+        name: "Goa",
+        duration: "4 Nights 5 Days",
+        discription: "Lying on the west coast, Goa is one of the smallest states in India known for its brilliant beaches, scrumptious food and Portuguese heritage. Panjim, the capital city located in the centre is well-connected with an international airport and roads and trains run from North to South part of Goa.",
+        sliderimg: 
+        [
+          "goa/slider/pic1.jpg",
+          "goa/slider/pic2.jpg",
+          "goa/slider/pic3.jpg",
+        ],
+        hotel:{
+          name: "Treehouse Silken Sands Resort",
+          hotelimg:
+          [
+            "goa/hotel/pic1.jpg",
+            "goa/hotel/pic2.jpg",
+            "goa/hotel/pic3.jpg",
+            "goa/hotel/pic4.jpg",
+          ]
+        },
+        flight:
+        {
+          count: "2",
+          timewithplace:
+          [
+            { from: " New Delhi (14:55)"  , to: "Hong Kong (06:10)"  },
+            { from: " Goa (12:10)"  , to: "New Delhi (15:10)"  }
+          ]
+        }
+        },
   
 
-]
+        {
+          name: "Mauritius",
+          duration: "6 Nights 7 Days",
+          discription: "With its white beaches and crystal clear sea water, Mauritius is indeed an idyllic destination where couples could enjoy a relaxing honeymoon. Indulge in a magical holiday in Mauritius, inclusive of exciting activities and stay at a beautiful hotel. Not only you will explore North, South and Ile Aux Cerf Islands with your partner but also you will get some fun freebies (hotel specific). You will surely enjoy a relaxing holiday and create lasting memories.",
+          sliderimg: 
+          [
+            "mauritius/slider/pic1.jpg",
+            "mauritius/slider/pic2.jpg",
+            "mauritius/slider/pic3.jpg",
+          ],
+          hotel:{
+            name: "Zilwa Attitude",
+            hotelimg:
+            [
+              "mauritius/hotel/pic1.jpg",
+              "mauritius/hotel/pic2.jpg",
+              "mauritius/hotel/pic3.jpg",
+              "mauritius/hotel/pic4.jpg",
+            ]
+          },
+          flight:
+          {
+            count: "2",
+            timewithplace:
+            [
+              { from: " New Delhi (21:55)"  , to: "Mauritius (06:10)"  },
+              { from: " Mauritius (17:10)"  , to: "New Delhi (20:10)"  }
+            ]
+          }
+          },
+
+          {
+            name: "Andaman",
+            duration: "5 Nights 6 Days",
+            discription: "Replete with picturesque shimmering beaches, lagoons of turquoise blue waters and a bit of history, Andaman & Nicobar Islands is a little slice of paradise tucked around 1,400 km away from the east coast of India. The union territory comprises a total of 572 islands, only 37 of which are inhabited and a few are open to the tourists.",
+            sliderimg: 
+            [
+              "andaman/slider/pic1.jpg",
+              "andaman/slider/pic2.jpg",
+              "andaman/slider/pic3.jpg",
+            ],
+            hotel:{
+              name: "Sea Shell",
+              hotelimg:
+              [
+                "andaman/hotel/pic1.jpg",
+                "andaman/hotel/pic2.jpg",
+                "andaman/hotel/pic3.jpg",
+                "andaman/hotel/pic4.jpg",
+              ]
+            },
+            flight:
+            {
+              count: "2",
+              timewithplace:
+              [
+                { from: " New Delhi (21:55)"  , to: "Port Blair (06:10)"  },
+                { from: " Port Blair (17:10)"  , to: "New Delhi (20:10)"  }
+              ]
+            }
+            },
+
+
+            {
+              name: "Maldives",
+              duration: "3 Nights 4 Days",
+              discription: "Head for a fancy journey on the sea with this Maldives cruise package. The luxury Costa Cruise ship will sail over the tranquil waters from Cochin to the Maldives. Equipped with numerous entertainment options, the cruise offers the best experiences to its guests. Savour delicious cuisines, experience amazing entertainment shows, enjoy games with your family or relax on the deck to enjoy stunning ocean and sunset views. The seven-day trip will leave you with memories of a lifetime.",
+              sliderimg: 
+              [
+                "maldives/slider/pic1.jpg",
+                "maldives/slider/pic2.jpg",
+                "maldives/slider/pic3.jpg",
+              ],
+              hotel:{
+                name: "Honors Residence",
+                hotelimg:
+                [
+                  "maldives/hotel/pic1.jpg",
+                  "maldives/hotel/pic2.jpg",
+                  "maldives/hotel/pic3.jpg",
+                  "maldives/hotel/pic4.jpg",
+                ]
+              },
+              flight:
+              {
+                count: "2",
+                timewithplace:
+                [
+                  { from: " New Delhi (21:55)"  , to: "Maldives (06:10)"  },
+                  { from: " Maldives (16:10))"  , to: "New Delhi (20:10)"  }
+                ]
+              }
+              },
+    
+
+              {
+                name: "Levi",
+                duration: "3 Nights 4 Days",
+                discription: "Levi is a fell located in Finnish Lapland, and the largest ski resort in Finland.The resort is located in Kittilä municipality and is served by Kittilä Airport and Kolari railway station.",
+                sliderimg: 
+                [
+                  "levi/slider/pic1.jpg",
+                  "levi/slider/pic2.jpg",
+                  "levi/slider/pic3.jpg",
+                ],
+                hotel:{
+                  name: "Break Sokos Hotel Levi",
+                  hotelimg:
+                  [
+                    "levi/hotel/pic1.jpg",
+                    "levi/hotel/pic2.jpg",
+                    "levi/hotel/pic3.jpg",
+                    "levi/hotel/pic4.jpg",
+                  ]
+                },
+                flight:
+                {
+                  count: "2",
+                  timewithplace:
+                  [
+                    { from: " New Delhi (21:55)"  , to: " Levi (06:10)"  },
+                    { from: "  Levi (16:10)"  , to: "New Delhi (20:10)"  }
+                  ]
+                }
+                },
+
+
+                {
+                  name: "Hallstatt",
+                  duration: "3 Nights 4 Days",
+                  discription: "Hallstatt is a village on Lake Hallstatt's western shore in Austria's mountainous Salzkammergut region. Its 16th-century Alpine houses and alleyways are home to cafes and shops. A funicular railway connects to Salzwelten, an ancient salt mine with a subterranean salt lake, and to Skywalk Hallstatt viewing platform. A trail leads to the Echern Valley glacier garden with glacial potholes and Waldbachstrub Waterfall.",
+                  sliderimg: 
+                  [
+                    "hallstatt/slider/pic1.jpg",
+                    "hallstatt/slider/pic2.jpg",
+                    "hallstatt/slider/pic3.jpg",
+                  ],
+                  hotel:{
+                    name: "Seehotel Gruener Baum",
+                    hotelimg:
+                    [
+                      "hallstatt/hotel/pic1.jpg",
+                      "hallstatt/hotel/pic2.jpg",
+                      "hallstatt/hotel/pic3.jpg",
+                      "hallstatt/hotel/pic4.jpg",
+                    ]
+                  },
+                  flight:
+                  {
+                    count: "2",
+                    timewithplace:
+                    [
+                      { from: "New Delhi (21:55)"  , to: "Hallstatt (06:10)"  },
+                      { from: "Hallstatt (16:10))"  , to: "New Delhi (20:10)"  }
+                    ]
+                  }
+                  },
+
+
+
+                  {
+                    name: "Mt. Pilatus",
+                    duration: "6 Nights 7 Days",
+                    discription: "Pilatus, also often referred to as Mount Pilatus, is a mountain massif overlooking Lucerne in Central Switzerland. It is composed of several peaks, of which the highest is named Tomlishorn. The whole mountain range stretches at least from the Lopper just opposite from Stansstad to the east as far as at least to the Mittaggüpfi (1,917 m [6,289 ft]) and the Risetestock (1,759 m [5,771 ft]) to the west on the border between LU and OW.",
+                    sliderimg: 
+                    [
+                      "pilatus/slider/pic1.jpg",
+                      "pilatus/slider/pic2.jpg",
+                      "pilatus/slider/pic3.jpg",
+                    ],
+                    hotel:{
+                      name: "Hotel Pilatus-Kulm",
+                      hotelimg:
+                      [
+                        "pilatus/hotel/pic1.jpg",
+                        "pilatus/hotel/pic2.jpg",
+                        "pilatus/hotel/pic3.jpg",
+                        "pilatus/hotel/pic4.jpg",
+                      ]
+                    },
+                    flight:
+                    {
+                      count: "2",
+                      timewithplace:
+                      [
+                        { from: " New Delhi (21:55)"  , to: "Mt.Pilatus (08:10)"  },
+                        { from: "Mt.Pilatus (18:10)"  , to: "New Delhi (20:10)"  }
+                      ]
+                    }
+                    },
+
+                    {
+                      name: "Innsbruck",
+                      duration: "6 Nights 4 Days",
+                      discription: "Located in the broad valley between high mountains, the so-called North Chain in the Karwendel Alps (Hafelekarspitze, 2,334 metres or 7,657 feet) to the north, and the Patscherkofel (2,246 m or 7,369 ft) and Serles (2,718 m or 8,917 ft) to the south. Innsbruck is an internationally renowned winter sports centre; it hosted the 1964 and 1976 Winter Olympics as well as the 1984 and 1988 Winter Paralympics. Innsbruck also hosted the first Winter Youth Olympics in 2012. The name translates as Inn Bridge",
+                      sliderimg: 
+                      [
+                        "innsbruck/slider/pic1.jpg",
+                        "innsbruck/slider/pic2.jpg",
+                        "innsbruck/slider/pic3.jpg",
+                      ],
+                      hotel:{
+                        name: "Adlers Hotel",
+                        hotelimg:
+                        [
+                          "innsbruck/hotel/pic1.jpg",
+                          "innsbruck/hotel/pic2.jpg",
+                          "innsbruck/hotel/pic3.jpg",
+                          "innsbruck/hotel/pic4.jpg",
+                        ]
+                      },
+                      flight:
+                      {
+                        count: "2",
+                        timewithplace:
+                        [
+                          { from: " New Delhi (21:55)"  , to: "Innsbruck (13:10)"  },
+                          { from: " Innsbruck (23:10)"  , to: "New Delhi (20:10)"  }
+                        ]
+                      }
+                      },
+
+                      {
+                        name: "New York",
+                        duration: "8 Nights 7 Days",
+                        discription: "New York City comprises 5 boroughs sitting where the Hudson River meets the Atlantic Ocean. At its core is Manhattan, a densely populated borough that’s among the world’s major commercial, financial and cultural centers. Its iconic sites include skyscrapers such as the Empire State Building and sprawling Central Park. Broadway theater is staged in neon-lit Times Square. ",
+                        sliderimg: 
+                        [
+                          "newyork/slider/pic1.jpg",
+                          "newyork/slider/pic2.jpg",
+                          "newyork/slider/pic3.jpg",
+                        ],
+                        hotel:{
+                          name: " Giorgio Hotel",
+                          hotelimg:
+                          [
+                            "newyork/hotel/pic1.jpg",
+                            "newyork/hotel/pic2.jpg",
+                            "newyork/hotel/pic3.jpg",
+                            "newyork/hotel/pic4.jpg",
+                          ]
+                        },
+                        flight:
+                        {
+                          count: "2",
+                          timewithplace:
+                          [
+                            { from: " New Delhi (21:55)"  , to: "New York (13:10)"  },
+                            { from: " New York (13:10)"  , to: "New Delhi (20:10)"  }
+                          ]
+                        }
+                        },
+
+
+                        {
+                          name: "Zurich",
+                          duration: "5 Nights 6 Days",
+                          discription: "The city of Zurich, a global center for banking and finance, lies at the north end of Lake Zurich in northern Switzerland. The picturesque lanes of the central Altstadt (Old Town), on either side of the Limmat River, reflect its pre-medieval history. Waterfront promenades like the Limmatquai follow the river toward the 17th-century Rathaus (town hall).",
+                          sliderimg: 
+                          [
+                            "zurich/slider/pic1.jpg",
+                            "zurich/slider/pic2.jpg",
+                            "zurich/slider/pic3.jpg",
+                          ],
+                          hotel:{
+                            name: "Radisson Blue Hotel",
+                            hotelimg:
+                            [
+                              "zurich/hotel/pic1.jpg",
+                              "zurich/hotel/pic2.jpg",
+                              "zurich/hotel/pic3.jpg",
+                              "zurich/hotel/pic4.jpg",
+                            ]
+                          },
+                          flight:
+                          {
+                            count: "2",
+                            timewithplace:
+                            [
+                              { from: "New Delhi (21:55)"  , to: "Zurich (06:10)"  },
+                              { from: "Zurich (20:10))"  , to: "New Delhi (20:10)"  }
+                            ]
+                          }
+                          },
+
+
+                          {
+                            name: "Shimla",
+                            duration: "4 Nights 5 Days",
+                            discription: "Shimla is the capital of the northern Indian state of Himachal Pradesh, in the Himalayan foothills. Once the summer capital of British India, it remains the terminus of the narrow-gauge Kalka-Shimla Railway, completed in 1903. It’s also known for the handicraft shops that line The Mall, a pedestrian avenue, as well as the Lakkar Bazaar, a market specializing in wooden toys and crafts.",
+                            sliderimg: 
+                            [
+                              "shimla/slider/pic1.jpg",
+                              "shimla/slider/pic2.jpg",
+                              "shimla/slider/pic3.jpg",
+                            ],
+                            hotel:{
+                              name: "Hotel Cardinal Royal ",
+                              hotelimg:
+                              [
+                                "shimla/hotel/pic1.jpg",
+                                "shimla/hotel/pic2.jpg",
+                                "shimla/hotel/pic3.jpg",
+                                "shimla/hotel/pic4.jpg",
+                              ]
+                            },
+                            flight:
+                            {
+                              count: "2",
+                              timewithplace:
+                              [
+                                { from: " New Delhi (21:55)"  , to: "Shimla(4:10)"  },
+                                { from: "Shimla (08:00)"  , to: "New Delhi (20:10)"  }
+                              ]
+                            }
+                            },
+
+                            {
+                              name: "Hallstatt",
+                              duration: "3 Nights 4 Days",
+                              discription: "",
+                              sliderimg: 
+                              [
+                                "sydney/slider/pic1.jpg",
+                                "sydney/slider/pic2.jpg",
+                                "sydney/slider/pic3.jpg",
+                              ],
+                              hotel:{
+                                name: "Honors Residence",
+                                hotelimg:
+                                [
+                                  "sydney/hotel/pic1.jpg",
+                                  "sydney/hotel/pic2.jpg",
+                                  "sydney/hotel/pic3.jpg",
+                                  "sydney/hotel/pic4.jpg",
+                                ]
+                              },
+                              flight:
+                              {
+                                count: "2",
+                                timewithplace:
+                                [
+                                  { from: " New Delhi (21:55)"  , to: "Maldives (06:10)"  },
+                                  { from: " Maldives (16:10))"  , to: "New Delhi (20:10)"  }
+                                ]
+                              }
+                              },
+
+
+                  ]
 
 }
 
