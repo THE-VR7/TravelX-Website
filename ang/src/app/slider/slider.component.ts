cb2d74ab6898ee8235d85ce99b1e5a0fde7b65c9
THE-VR7 <vineetrathor57@gmail.com>
2020-04-08 19:28:30 +0530

    s

--- ang/src/app/slider/slider.component.ts
+++ ang/src/app/slider/slider.component.ts
@@ -7,8 +7,7 @@ import {ConfigService} from '../config.service';
   styleUrls: ['./slider.component.css']
 })
 export class SliderComponent implements OnInit {
-  banner =  {
-  };
+  banner =  {};
 
 
 
