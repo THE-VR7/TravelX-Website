202a2334234b6b929d2c44cf5726d28cc38618c5
THE-VR7 <vineetrathor57@gmail.com>
2020-04-28 01:16:43 +0530

    P

--- ang/src/app/signup/signup.component.ts
+++ ang/src/app/signup/signup.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit ,ViewChild} from '@angular/core';
 import { FormGroup, FormBuilder, Validators, NgForm } from '@angular/forms';
 import { AuthenticationService } from '../authentication.service';
 import { Router } from '@angular/router';
@@ -11,17 +11,98 @@ import { Router } from '@angular/router';
 export class SignupComponent implements OnInit {
 
   signupform : FormGroup;
+  // @ViewChild('fform',{static: true}) signupformDirective;
 
-  constructor( private fb : FormBuilder, private auth: AuthenticationService, private router:Router ) { }
+  public formErrors:any = {
+    'username': '',
+    'name': '',
+    'password': '',
+    'email': ''
+  };
+
+  public d1 = [
+  ]
+
+ public validationMessages = {
+    'username': {
+      'required':      'User Name is required.',
+      'minlength':     'User Name must be at least 3 characters long.',
+      'maxlength':     'User Name cannot be more than 25 characters long.'
+    },
+    'name': {
+      'required':      ' Name is required.',
+      'minlength':     ' Name must be at least 2 characters long.',
+      'maxlength':     ' Name cannot be more than 25 characters long.'
+    },
+    'password': {
+      'required':      'Password is required.',
+      'minlength':     'Password must be at least 6 characters long.',
+    },
+    'email': {
+      'required':      'Email is required.',
+      'email':         'Email not in valid format.'
+    },
+  };
+
+
+
+  constructor( private fb : FormBuilder, private auth: AuthenticationService, private router:Router ) { 
+    this.createform();
+
+  }
 
   ngOnInit() {
-    this.signupform = this.fb.group({
-      'username': [null,[Validators.required,Validators.minLength(3)]],
-      'password': [null,Validators.required],
-      'name': [null,Validators.required],
-      'email':[null,[Validators.required,Validators.email]]
-    });
   }
+
+
+  createform()
+  {
+    this.signupform = this.fb.group({
+      'username': ['',[Validators.required,Validators.minLength(3),Validators.maxLength(25)]],
+      'password': ['',[Validators.required,Validators.minLength(6)]],
+      'name': ['',[Validators.required, Validators.minLength(2), Validators.maxLength(25)]],
+      'email':['',[Validators.required,Validators.email]]
+    });
+
+    this.signupform.valueChanges
+    .subscribe(data => {
+      this.onvaluechange(data);
+      console.log(this.formErrors);
+    });
+
+    this.onvaluechange();
+
+  }
+
+
+
+  onvaluechange(data?: any)
+  {
+    if(!this.signupform) {return;}
+    const form = this.signupform;
+
+    for(const field in this.formErrors)
+    {
+      if(this.formErrors.hasOwnProperty(field))
+      {
+        this.formErrors[field] = '';
+        const control = form.get(field);
+        if(control && control.dirty && !control.valid)
+        {
+          const messages = this.validationMessages[field];
+          for(const key in control.errors)
+          {
+            if(control.errors.hasOwnProperty(key))
+            {
+              this.formErrors[field] += messages[key] + ' ';
+            }
+          }
+        }
+      }
+    }
+
+  }
+
 
 
   signup(formData : NgForm):void
@@ -31,5 +112,7 @@ export class SignupComponent implements OnInit {
       console.log(`added user ${JSON.stringify(user)}`);
       this.router.navigate(['login']);
     });
+    this.signupform.reset();
+    // this.signupformDirective.resetForm();
   }
 }
